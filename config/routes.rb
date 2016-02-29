572bcbea0c800807253e1002f7b35810ab4c4f37
Alan Koger <adkoge@gmail.com>
2016-02-29 19:26:34 +0000

    Create lessons model and databas. Set up routes and styling.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Flixter::Application.routes.draw do
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
   namespace :instructor do
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
+    resources :sections, only: []
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
