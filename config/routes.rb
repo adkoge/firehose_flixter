f4030a034449e22f556fb20e235e577ebce3500e
Alan Koger <adkoge@gmail.com>
2016-02-29 17:07:18 +0000

    Add sections to instructor course page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Flixter::Application.routes.draw do
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
+    end
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
