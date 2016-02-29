8f9d2bb6a122deb4d2f221c8055ffe5527d3ca52
Alan Koger <adkoge@gmail.com>
2016-02-29 00:53:17 +0000

    Add course controller and list courses.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Flixter::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
   end
