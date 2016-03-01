3b19fb8c610ef8df7d1bee5ce603fec1271e873c
Alan Koger <adkoge@gmail.com>
2016-03-01 01:21:42 +0000

    Add courses.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
   has_many :courses
-
-  mount_uploader :image, ImageUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
